5610906d4bf107cdf0e08e13bf69419b80341e63
Jacob Corum <jacob.w.corum@gmail.com>
2022-05-12 10:28:09 -0600

    added bootstrap and tinkered with some styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {ServerComponent} from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
+  // add other modules to this module to keep things lean.
   imports: [
     BrowserModule
   ],
