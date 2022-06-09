ef955917fcaafca4d04508750d0ec72ba8e1697b
Jacob Corum <jacob.w.corum@gmail.com>
2022-06-09 13:24:39 -0600

    added more routing

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -1,23 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { UsersComponent } from './users/users.component';
-import { ServersComponent } from './servers/servers.component';
-import { UserComponent } from './users/user/user.component';
-import { EditServerComponent } from './servers/edit-server/edit-server.component';
-import { ServersService } from './servers/servers.service';
-import {RouterModule, Routes} from '@angular/router';
-
-const appRoutes: Routes = [
-	{ path: '', component: HomeComponent },
-	{ path: 'users', component: UsersComponent },
-	{ path: 'users/:id/:name', component: UserComponent },
-	{ path: 'servers', component: ServersComponent }
-]
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { UsersComponent } from "./users/users.component";
+import { ServersComponent } from "./servers/servers.component";
+import { UserComponent } from "./users/user/user.component";
+import { EditServerComponent } from "./servers/edit-server/edit-server.component";
+import { ServersService } from "./servers/servers.service";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -27,14 +20,11 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServersComponent
+    ServersComponent,
+    PageNotFoundComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-		RouterModule.forRoot(appRoutes)
-  ],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [ServersService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
