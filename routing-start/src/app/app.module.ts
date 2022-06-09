ad344c8a49f6be17f815c56711cbda0ec547234d
Jacob Corum <jacob.w.corum@gmail.com>
2022-06-09 09:35:26 -0600

    added some routing

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -9,8 +9,15 @@ import { UsersComponent } from './users/users.component';
 import { ServersComponent } from './servers/servers.component';
 import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
-import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+	{ path: '', component: HomeComponent },
+	{ path: 'users', component: UsersComponent },
+	{ path: 'users/:id/:name', component: UserComponent },
+	{ path: 'servers', component: ServersComponent }
+]
 
 @NgModule({
   declarations: [
@@ -20,11 +27,12 @@ import { ServersService } from './servers/servers.service';
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServersComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+		RouterModule.forRoot(appRoutes)
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
