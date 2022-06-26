ee2ebd5d33de95ce80816e9460bf787a3cd8bb08
Jacob Corum <jacob.w.corum@gmail.com>
2022-06-25 23:55:24 -0600

    completed some auth

---
+++ recipe-shopping-list/src/app/auth/auth.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { catchError } from "rxjs/operators";
+import { throwError } from "rxjs";
+
+export interface AuthResponseData {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
+
+@Injectable({
+  providedIn: "root",
+})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  signup(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDCG_QCjm5zrlsTBJ4xUYnNkS-6b-MGxdc",
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(catchError(this.handleError));
+  }
+
+  login(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDCG_QCjm5zrlsTBJ4xUYnNkS-6b-MGxdc",
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = "An unknown error ocurred!";
+    if (!errorRes || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      case "EMAIL_EXISTS":
+        errorMessage = "This email exists already";
+        break;
+      case "EMAIL_NOT_FOUND":
+        errorMessage = "This email does not exist";
+        break;
+      case "INVALID_PASSWORD":
+        errorMessage = "This password is not correct";
+        break;
+    }
+    return throwError(errorMessage);
+  }
+}
