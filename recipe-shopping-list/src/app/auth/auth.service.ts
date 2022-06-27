8d928044122d634fc8f1fd0aa8f4e9779bb925bf
Jacob Corum <jacob.w.corum@gmail.com>
2022-06-27 10:45:55 -0600

    authentication continued

--- recipe-shopping-list/src/app/auth/auth.service.ts
+++ recipe-shopping-list/src/app/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
-import { catchError } from "rxjs/operators";
-import { throwError } from "rxjs";
+import { catchError, tap } from "rxjs/operators";
+import { throwError, BehaviorSubject } from "rxjs";
+import { User } from "./user.model";
 
 export interface AuthResponseData {
   idToken: string;
@@ -16,6 +17,8 @@ export interface AuthResponseData {
   providedIn: "root",
 })
 export class AuthService {
+  user = new BehaviorSubject<User>(null);
+
   constructor(private http: HttpClient) {}
 
   signup(email: string, password: string) {
@@ -28,7 +31,17 @@ export class AuthService {
           returnSecureToken: true,
         }
       )
-      .pipe(catchError(this.handleError));
+      .pipe(
+        catchError(this.handleError),
+        tap((resData) => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
+        })
+      );
   }
 
   login(email: string, password: string) {
@@ -42,6 +55,17 @@ export class AuthService {
         }
       )
       .pipe(catchError(this.handleError));
+  }
+
+  handleAuthentication(
+    email: string,
+    userid: string,
+    token: string,
+    expiresIn: number
+  ) {
+    const expirationData = new Date(new Date().getTime() + expiresIn * 1000);
+    const user = new User(email, userid, token, expirationData);
+    this.user.next(user);
   }
 
   private handleError(errorRes: HttpErrorResponse) {
